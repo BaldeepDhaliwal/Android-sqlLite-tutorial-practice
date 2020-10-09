180735901aec9054b837f7ecaa8feb882cf43864
Baldeep <dhaliwal.baldeep2@gmail.com>
2020-10-09 16:17:34 -0700

    covered Inserting values into SQLite db

--- Desktop/cs/app/src/main/java/com/example/sqlitetutorialfollowalongpractice/DatabaseHelper.java
+++ Desktop/cs/app/src/main/java/com/example/sqlitetutorialfollowalongpractice/DatabaseHelper.java
@@ -1,35 +1,17 @@
 package com.example.sqlitetutorialfollowalongpractice;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-
-///////////////Notes////////////////////////////
-
-//SQLiteOpenHelper is used to handle SQLite
-
-
-//db format
-//Table Name: Student.db
 //Fields: ID(pk), Name, Surname, Marks
 
-
-
-//////////////////////////////////////////////////
-
-//sqlLite db name not case sensetive.
 public class DatabaseHelper extends SQLiteOpenHelper {
-    //placeholder to hold db name
     public static final String DATABASE_NAME = " Student.db";
-    //place holder to hold table name
     public static final String TABLE_NAME = "Student_table";
-    //create placeholders to hold Column Names
-    /*
-    ie ID, Name, Surname, Marks
-    */
     public static final String COL_1 = "ID";
     public static final String COL_2 = "Name";
     public static final String COL_3 = "Surname";
@@ -38,21 +20,31 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     //create db
     public DatabaseHelper(@Nullable Context context) {
         super(context, DatabaseHelper.DATABASE_NAME, null, 1);
-
     }
 
-    //Create tables
+    //Create table
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        //NOTE: SQLITE data types: NULL, INTEGER, REAL (FLOAT), TEXT, BLOB
-
-
-        //execute sql CREATE TABLE query
+        //execute sql query
         sqLiteDatabase.execSQL("CREATE TABLE " + DatabaseHelper.TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Surname TEXT, Marks TEXT)");
     }
 
+    //
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+DatabaseHelper.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
 
+    //insert data into table
+    public boolean insertData(String name, String surname, String mark){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Name", name);
+        contentValues.put("Surname",surname);
+        contentValues.put("Marks",mark);
+        //db.execSQL("INSERT INTO "+ DatabaseHelper.TABLE_NAME+" (Name,Surname,Marks) VALUES ("+name+","+surname+","+mark+")");
+        long ret = db.insert(DatabaseHelper.TABLE_NAME, null, contentValues);
+        return ret != -1;
     }
 }
--- Desktop/cs/app/src/main/java/com/example/sqlitetutorialfollowalongpractice/MainActivity.java
+++ Desktop/cs/app/src/main/java/com/example/sqlitetutorialfollowalongpractice/MainActivity.java
@@ -2,14 +2,49 @@ package com.example.sqlitetutorialfollowalongpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    DatabaseHelper db;
+    EditText name;
+    EditText surName;
+    EditText mark;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create db
+        db = new DatabaseHelper(this);
+
+        //Make widgets
+        button = (Button) findViewById(R.id.addDataButton);
+        name = (EditText) findViewById(R.id.nameEditText);
+        surName = (EditText) findViewById(R.id.surnameEditText);
+        mark = (EditText) findViewById(R.id.marksEditBox);
+
+        //when button clicked get text values from editTexts and insert data into db.
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //insert Name, Surname, Mark tuple into db.
+               boolean isInserted =  db.insertData(name.getText().toString(), surName.getText().toString(),mark.getText().toString());
+
+               //Show message indicating tuple insertion success or failure
+               if (isInserted){
+                   Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+               }
+               else{
+                   Toast.makeText(MainActivity.this,"Data insertion failed", Toast.LENGTH_LONG).show();
+               }
+            }
+        });
     }
 
 
